0c4dbd6619d82b9806a60526fe1c6f4e2e36316b
Murad Nurmukhamedov <mns130691@gmail.com>
2020-07-22 16:52:32 +0300

    add session variable

--- index.php
+++ index.php
@@ -1,5 +1,39 @@
+<?php
 
+if (isset($_POST["add"]))
+{
+    if (isset($_SESSION["cart"]))
+    {
+        $array_id = array_column($_SESSION["cart"], "product_id");
 
+        //check for already being product in cart
+        if (in_array($_POST["product_id"], $array_id))
+        {
+         echo "<script>alert('Товар уже добавлен')</script>";
+         echo "<script>window.location = 'index.php'</script>";
+        }
+        else 
+        {
+         $count = count($_SESSION["cart"]);
+         $item_array = array(
+            'product_id' => $_POST["product_id"],
+         );
+         $_SESSION["cart"][$count] = $item_array;
+        }
+
+    }
+    else 
+    {
+      $item_array = array(
+         'product_id' => $_POST["product_id"],
+      );
+
+      //create new session variable
+      $_SESSION["cart"][0] = $item_array;
+    }
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,8 +54,10 @@
           <span class="price"><?php echo $product_cost; ?></span>
         </h4>
         <button type="submit" name="add">Add <i class="fas fa-shopping-cart"></i></button>
-<!-- create a hidden propertie for $_REQUEST or $_POST. we need that later -->
-        <input type="hidden" name="product_id" value="<?php echo $product_id; ?>"> 
+        
+        <!-- create a hidden propertie for $_REQUEST or $_POST. we need that later -->
+        <input type="hidden" name="product_id" value="<?php echo $product_id; ?>">
+        
       </div>       
 </form>
     
