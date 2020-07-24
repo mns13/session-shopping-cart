a0aec2e8cae0ad92c4ed3eaf52df7246df85065b
Murad Nurmukhamedov <mns130691@gmail.com>
2020-07-24 19:06:04 +0300

    change file

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <?php
 
 if (isset($_POST["add"]))
-{
-    if (isset($_SESSION["cart"]))
     {
-        $array_id = array_column($_SESSION["cart"], "product_id");
+        if (isset($_SESSION["cart"]))
+            {
+                $array_id = array_column($_SESSION["cart"], "product_id");
 
-        //check for already being product in cart
-        if (in_array($_POST["product_id"], $array_id))
-        {
-         echo "<script>alert('Товар уже добавлен')</script>";
-         echo "<script>window.location = 'index.php'</script>";
-        }
+                //check for already being product in cart
+                if (in_array($_POST["product_id"], $array_id))
+                    {
+                     echo "<script>alert('Товар уже добавлен')</script>";
+                     echo "<script>window.location = 'index.php'</script>";
+                    }
+                else 
+                    {
+                     $count = count($_SESSION["cart"]);
+                     $item_array = array(
+                        'product_id' => $_POST["product_id"],
+                     );
+                     $_SESSION["cart"][$count] = $item_array;
+                    }
+
+            }
         else 
-        {
-         $count = count($_SESSION["cart"]);
-         $item_array = array(
-            'product_id' => $_POST["product_id"],
-         );
-         $_SESSION["cart"][$count] = $item_array;
-        }
+            {
+              $item_array = array(
+                 'product_id' => $_POST["product_id"],
+              );
 
+              //create new session variable
+              $_SESSION["cart"][0] = $item_array;
+            }
     }
-    else 
-    {
-      $item_array = array(
-         'product_id' => $_POST["product_id"],
-      );
-
-      //create new session variable
-      $_SESSION["cart"][0] = $item_array;
-    }
-}
 
 ?>
 
